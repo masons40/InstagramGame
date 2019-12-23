6461c9e25486eb8ff3a61e3b4ac79b93272366fb
Mason <masonsmith40@hotmail.co.uk>
2019-12-23 23:39:51 +0000

    Spotify data added

--- src/main/java/com/instagramGame/InstagramGame/InstagramGameApplication.java
+++ src/main/java/com/instagramGame/InstagramGame/InstagramGameApplication.java
@@ -2,12 +2,15 @@ package com.instagramGame.InstagramGame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import spotify.GetSeveralArtistsExample;
 
 @SpringBootApplication
 public class InstagramGameApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(InstagramGameApplication.class, args);
-	}
 
+		GetSeveralArtistsExample.getListOfCategories_Sync();
+	}
 }
---
+++ src/main/java/spotify/ClientCredentialsExample.java
@@ -0,0 +1,65 @@
+package spotify;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+public class ClientCredentialsExample {
+    private static final String clientId = "85f0169c7a094a83bbea9df0fffade79";
+    private static final String clientSecret = "93d61566377c4904b27e6048a83ac49b";
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
+            .build();
+    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
+            .build();
+
+    public static ClientCredentials getClientCredentialsToken() {
+        try {
+            return clientCredentialsRequest.execute();
+        } catch (IOException | SpotifyWebApiException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public static void clientCredentials_Sync() {
+        try {
+            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+
+            // Set access token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+            System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+        } catch (IOException | SpotifyWebApiException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void clientCredentials_Async() {
+        try {
+            final CompletableFuture<ClientCredentials> clientCredentialsFuture = clientCredentialsRequest.executeAsync();
+
+            // Thread free to do other tasks...
+
+            // Example Only. Never block in production code.
+            final ClientCredentials clientCredentials = clientCredentialsFuture.join();
+
+            // Set access token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+            System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+        } catch (CompletionException e) {
+            System.out.println("Error: " + e.getCause().getMessage());
+        } catch (CancellationException e) {
+            System.out.println("Async operation cancelled.");
+        }
+    }
+}

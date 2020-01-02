df34aa765d2221d810bc0a02399298ae6ac2d411
Mason <masonsmith40@hotmail.co.uk>
2020-01-02 15:40:57 +0000

    script updates

--- src/main/resources/static/js/InstagramGame.js
+++ src/main/resources/static/js/InstagramGame.js
@@ -1,9 +1,10 @@
 
 $("document").ready(function(){
 
-
     var data_list , successJson;
     var popularity = [];
+    var score=0;
+    var artistOne, artistTwo;
 
     $('#play-button').click(function () {
 
@@ -47,8 +48,10 @@ $("document").ready(function(){
 
     $("#secondary-play-button").click(function () {
         $("#secondary-play-button-area").slideToggle();
-        getArtistInfo(data_list[Math.floor(Math.random() * Object.keys(data_list).length)], "#username_one","#image_one");
-        getArtistInfo(data_list[Math.floor(Math.random() * Object.keys(data_list).length)], "#username_two","#image_two");
+        findArtist(artistOne);
+        findArtist(artistTwo);
+        getArtistInfo(artistOne, "#username_one","#image_one");
+        getArtistInfo(artistTwo, "#username_two","#image_two");
     });
 
 
@@ -58,20 +61,58 @@ $("document").ready(function(){
             $.each(data, function (key, value) {
                 if(key==="name"){
                     $(user_name).text(value);
-                    console.log(value);
                 }else if(key==="profilePic"){
+                    console.log("pic", value);
                     $(user_pic).attr("src", value);
                 }else if(key==="popularity"){
                     popularity.push(value);
-                    console.log(value);
                 }
 
-                console.log();
             });
         });
     }
 
     $('.profile_followers').click(function () {
-       console.log(popularity);
+       var profile = $(this).attr("data-id");
+       if(profile==="followers_one"){
+           if(popularity[0] >= popularity[1]){
+               $(this).css("border", "6px solid #68d968");
+               score+=1;
+               $("#user-score").text(score);
+           }else{
+               $(this).css("border", "6px solid #d92929");
+           }
+           deleteVal(data_list, artistOne);
+           findArtist(artistOne);
+
+       }else{
+           if(popularity[1] >= popularity[0]){
+               $(this).css("border", "6px solid #68d968");
+               score+=1;
+               $("#user-score").text(score);
+           }else{
+               $(this).css("border", "6px solid #d92929");
+           }
+           deleteVal(data_list, artistTwo);
+           findArtist(artistTwo);
+       }
+
+       $(this).css("border", "none");
     });
+
+    function deleteVal(arr, value){
+        for( var i = 0; i < arr.length; i++){
+            if ( arr[i] === value) {
+                arr[i] = "empty";
+            }
+        }
+    }
+
+    function findArtist(val) {
+        if(val === artistOne){
+            artistOne = data_list[Math.floor(Math.random() * data_list.length)];
+        }else{
+            artistTwo = data_list[Math.floor(Math.random() * data_list.length)];
+        }
+    }
 });
